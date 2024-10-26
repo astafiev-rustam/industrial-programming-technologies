34523d0540f36eb6815273be2c1cd4b6008b1ba5
astafiev-rustam <astafev@mirea.ru>
2024-10-26 20:21:40 +0300

    Create ex1.cpp

---
+++ S03/L05/ex1.cpp
@@ -0,0 +1,27 @@
+#include <QApplication>
+#include <QWidget>
+#include <QListView>
+#include <QStringListModel>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QWidget* widget = new QWidget;
+    widget->setWindowTitle("INPRO.COM");
+    widget->setMinimumHeight(150);
+    widget->setMinimumWidth(250);
+
+    // определяем данные для модели
+    QStringList list = { "Tom", "Bob", "Sam" };
+    // определяем модель
+    QStringListModel *model = new QStringListModel(list);
+
+    // определяем представление
+    QListView *view = new QListView(widget);
+    // устанавливаем модель для представления
+    view->setModel(model);
+
+    widget->show();
+    return a.exec();
+}
